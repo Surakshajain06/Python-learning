10613581a95f590d565ecf32a981b936f0b2cc8b
Suraksha Jain <surakshajain06@gmail.com>
2025-04-08 10:07:35 +0530

    Add files via upload

---
+++ Banking systems/testBankAcct.py
@@ -0,0 +1,9 @@
+from Bank_Acoount import bankaccount,savingsAcct
+shree = savingsAcct("shree", 1000)
+shree.deposit(1000)
+shree.GetBalance()
+
+suru= savingsAcct("suru", 1000)
+suru.withdrawal(500)
+
+shree.Transfer(suru,1000)
